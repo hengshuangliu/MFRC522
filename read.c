93931cdc277f99af592b35ef85e8e491862f4a2a
hengshuangliu <767924520@qq.com>
2016-04-16 14:45:13 +0800

    Update read.c

--- read.c
+++ read.c
@@ -9,11 +9,11 @@ int main(){
   unsigned char id[10];
   int i;
   if (!bcm2835_init()) return -1;
-  RFID_init();
+  MFRC522_init();
  
   while(1){
-    if (findCard(0x52,&s)==MI_OK){
-       if ( anticoll(id)==MI_OK){
+    if (RFID_findCard(0x52,&s)==MI_OK){
+       if ( RFID_anticoll(id)==MI_OK){
          for(i=0;i<5;i++)  printf("%d ",id[i]);
          printf("\n");
        } else printf("ERR\n");
